5fb94d355e04c4eeadc10a5a35a94f12da0e99ae
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2026-08-20 10:45:27 +0300

    SF

--- frontend4/src/components/product/ProductList.vue
+++ frontend4/src/components/product/ProductList.vue
@@ -1,21 +1,35 @@
 <template>
-  <div class="p-2 d-flex flex-wrap justify-content-around">
-    <Spinner :isLoading="productStore.isLoading" />
-    <ProductListItem
-      v-for="product in productStore.products"
-      :key="product.id"
-      :product="product"
-    />
+  <div>
+    <div class="p-2 d-flex flex-wrap justify-content-around">
+      <Spinner :isLoading="productStore.isLoading" />
+      <ProductListItem
+        v-for="product in productStore.products.slice(0, data.showItem)"
+        :key="product.id"
+        :product="product"
+      />
+    </div>
+    <div
+      class="align-middle text-center"
+      v-if="data.showItem <= productStore.products.length"
+    >
+      <button class="btn btn-dark" @click="data.showItem += 2">
+        <i class="bi bi-arrow-clockwise"></i> Load More
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { useProductStore } from "@/stores/useProductStore";
-import { onMounted } from "vue";
+import { onMounted, reactive } from "vue";
 import ProductListItem from "./ProductListItem.vue";
 import Spinner from "../layouts/Spinner.vue";
 
 const productStore = useProductStore();
+
+const data = reactive({
+  showItem: 2,
+});
 
 onMounted(() => {
   productStore.fetchProducts();
